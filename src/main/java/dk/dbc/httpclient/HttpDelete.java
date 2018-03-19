73b939c9dc0a97c0b5d229c692b793123e9d0ed5
Jens Stein Jørgensen <jsj@dbc.dk>
2018-03-19 16:04:23 +0100

    Remove dataio from paths

--- src/main/java/dk/dbc/httpclient/HttpDelete.java
+++ src/main/java/dk/dbc/httpclient/HttpDelete.java
@@ -19,39 +19,37 @@
  * along with DataIO.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package dk.dbc.dataio.commons.utils.httpclient;
+package dk.dbc.httpclient;
 
 import javax.ws.rs.core.Response;
 import java.util.Arrays;
 
 /**
- * HTTP GET request representation
+ * HTTP DELETE request representation
  *
  * <p>
  * Example:
  * <pre>
  * {@code
- * final HttpGet httpGet = new HttpGet(client)
+ * final HttpDelete httpDelete = new HttpDelete(client)
  *          .withBaseUrl("http://localhost:8080")
- *          .withPathElements("path", "to", "resource")
- *          .withQueryParameter("key", "value")
- *          .withHeader("Accept", "text/html");
+ *          .withPathElements("path", "to", "resource");
  * }
  * </pre>
  */
-public class HttpGet extends HttpRequest<HttpGet> {
-    public HttpGet(HttpClient httpClient) {
+public class HttpDelete extends HttpRequest<HttpDelete> {
+    public HttpDelete(HttpClient httpClient) {
         super(httpClient);
     }
 
     @Override
     public Response call() {
-        return HttpClient.doGet(this);
+        return HttpClient.doDelete(this);
     }
 
     @Override
     public String toString() {
-        return "HttpGet{" +
+        return "HttpDelete{" +
                 "headers=" + headers +
                 ", queryParameters=" + queryParameters +
                 ", baseUrl='" + baseUrl + '\'' +
