7c256ed7e8da1eefec762b3e46f7771638f4db5f
Jens Stein Jørgensen <jsj@dbc.dk>
2018-08-31 11:28:24 +0200

    Avoid putting trailing slashes on urls
    A trailing slash was put on urls without path params. This lead to
    some resources working because of redirection on the endpoint side and
    some resources not working because of the incorrect url.

---
+++ src/test/java/dk/dbc/httpclient/HttpClientTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt
+ */
+
+package dk.dbc.httpclient;
+
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class HttpClientTest {
+    @Test
+    public void test_setPathParametersOnWebTarget()
+            throws URISyntaxException {
+        Client client = HttpClient.newClient();
+        HttpClient httpClient = new HttpClient(client);
+        HttpGet httpGet = new HttpGet(httpClient).withPathElements(
+            "square", "pants");
+        WebTarget target = httpGet.getHttpClient().getClient()
+            .target("sponge/bob");
+        target = HttpClient.setPathParametersOnWebTarget(
+            httpGet.getPathElements(), target);
+
+        assertThat(target.getUri(), is(new URI("sponge/bob/square/pants")));
+    }
+
+    @Test
+    public void test_setPathParametersOnWebTarget_noTrailingSlash()
+            throws URISyntaxException {
+        Client client = HttpClient.newClient();
+        HttpClient httpClient = new HttpClient(client);
+        HttpGet httpGet = new HttpGet(httpClient);
+        WebTarget target = httpGet.getHttpClient().getClient()
+            .target("sponge/bob");
+        target = HttpClient.setPathParametersOnWebTarget(
+            httpGet.getPathElements(), target);
+
+        assertThat(target.getUri(), is(new URI("sponge/bob")));
+    }
+}
